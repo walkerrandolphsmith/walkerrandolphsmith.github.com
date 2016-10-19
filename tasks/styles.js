c35b2d23e715dff723021a7405ec32bf5fc6652a
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-10-19 12:31:05 -0400

    Minify css.

--- tasks/styles.js
+++ tasks/styles.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var less = require('gulp-less');
 var rename = require('gulp-rename');
 var LessAutoprefix = require('less-plugin-autoprefix');
+var cleanCSS = require('gulp-clean-css');
 var config = require("./config");
 
 gulp.task('styles', () => {
@@ -12,5 +13,6 @@ gulp.task('styles', () => {
             ]
         }))
         .pipe(rename(config.styles.name))
+        .pipe(cleanCSS({ compatibility: 'ie8' }))
         .pipe(gulp.dest(config.styles.dest));
 });
