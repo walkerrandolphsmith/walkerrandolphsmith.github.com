b46a5f280a0deef5936f297fde617a5b7386e3db
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-10-21 16:35:32 -0400

    All pages have a floating nav.

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,20 @@
 import base from './base';
+import $ from 'jquery';
+
+const HERO_HEIGHT = 527;
+$(function () {
+    const $window = $(window);
+    const $header = $($('header.blog-info')[0]);
+    let prev = 0;
+    let lastScrollTop = 0;
+    $window.scroll(() => {
+        lastScrollTop = $window.scrollTop();
+
+        if (lastScrollTop > HERO_HEIGHT) {
+            $header.toggleClass('hidden', lastScrollTop > prev);
+            prev = lastScrollTop;
+        } else {
+            $header.addClass('hidden');
+        }
+    })
+});
--- src/app/post.js
+++ src/app/post.js
@@ -27,34 +27,32 @@ export default (() => {
         let prev = 0;
         let lastScrollTop = 0;
 
-        if (location.pathname.match(/post/)) {
-            $window.scroll(() => {
-                lastScrollTop = $window.scrollTop();
+        $window.scroll(() => {
+            lastScrollTop = $window.scrollTop();
 
-                const currentPosition = lastScrollTop + windowHeight;
-                const percentComplete = currentPosition / documentHeight;
-                $meter.css({width: (percentComplete * 100) + "%"});
+            const currentPosition = lastScrollTop + windowHeight;
+            const percentComplete = currentPosition / documentHeight;
+            $meter.css({width: (percentComplete * 100) + "%"});
 
-                if ($postBottom[0].getBoundingClientRect().bottom < (window.innerHeight / 2)) {
-                    $default.hide();
-                    $relatedPosts.fadeIn('slow')
-                }
-                else {
-                    $default.fadeIn('slow');
-                    $relatedPosts.hide();
-                }
+            if ($postBottom[0].getBoundingClientRect().bottom < (window.innerHeight / 2)) {
+                $default.hide();
+                $relatedPosts.fadeIn('slow')
+            }
+            else {
+                $default.fadeIn('slow');
+                $relatedPosts.hide();
+            }
 
-                if (lastScrollTop > HERO_HEIGHT) {
-                    $header.toggleClass('hidden', lastScrollTop > prev);
-                    prev = lastScrollTop;
-                } else {
-                    $header.addClass('hidden');
-                    const opacity = (lastScrollTop / HERO_HEIGHT) + 0.3;
-                    $background.css({ opacity: opacity });
-                    $title.css({ opacity: 1 - opacity });
-                }
-            });
-        }
+            if (lastScrollTop > HERO_HEIGHT) {
+                $header.toggleClass('hidden', lastScrollTop > prev);
+                prev = lastScrollTop;
+            } else {
+                $header.addClass('hidden');
+                const opacity = (lastScrollTop / HERO_HEIGHT) + 0.3;
+                $background.css({ opacity: opacity });
+                $title.css({ opacity: 1 - opacity });
+            }
+        });
     });
 
     const backToTop = () => {
