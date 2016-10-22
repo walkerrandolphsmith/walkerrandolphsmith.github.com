f4c97f4869fef56d12ee6e3b69cbf483046a1468
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-10-22 18:46:16 -0400

    Open all external links in a new tab.

--- src/app/base.js
+++ src/app/base.js
@@ -53,6 +53,17 @@ export default (() => {
         $('.open').on('click', () => {
             $drawer.addClass('show');
         });
+
+        $('a').each(function() {
+            var a = new RegExp('/' + window.location.host + '/');
+            if(!a.test(this.href)) {
+                $(this).click(function(event) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                    window.open(this.href, '_blank');
+                });
+            }
+        });
     });
     
     request
