ea3dd7c87cfb6951a151dd5c8f53b1c7d5717fa7
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-10-23 19:37:43 -0400

    Remove deploy task. Favor the terms pre-publish and publish.

--- tasks/deploy.js
+++
@@ -1,8 +0,0 @@
-const gulp = require('gulp');
-const shell = require('gulp-shell');
-
-gulp.task('deploy', ['build'], function() {
-    shell.task([
-        'git push origin `git subtree split --prefix build develop`:master --force'
-    ]);
-});
