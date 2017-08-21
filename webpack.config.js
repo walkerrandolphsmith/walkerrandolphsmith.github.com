4b3b9be132f865fbb14d82062f8e28644a67c000
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2017-08-20 20:01:52 -0400

    Webpack oputputs to docs

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var webpack = require('webpack');
 var path = require('path');
 var nodeModulesPath = path.resolve(__dirname, 'node_modules');
-var buildPath = path.resolve(__dirname, 'build');
+var buildPath = path.resolve(__dirname, 'docs');
 var mainPath = path.resolve(__dirname, 'src', 'app', 'index.js');
 var postPath = path.resolve(__dirname, 'src', 'app', 'post.js');
 
