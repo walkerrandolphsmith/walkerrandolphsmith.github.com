66ad3d2465ba0ef700bb99b15f2ba01c03d34ad3
walkersmith <wsmith@bluelinesolutions.org>
2025-10-14 18:18:55 -0400

    remove stale projects

--- ingress-controller/install.sh
+++
@@ -1,4 +0,0 @@
-helm upgrade ingress-nginx ingress-nginx/ingress-nginx \
-  --namespace ingress-nginx \
-  --set controller.service.externalTrafficPolicy=Local \
-  --set controller.watchNamespace=\"\"
